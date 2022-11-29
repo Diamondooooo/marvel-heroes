011dca6e3db74bcad38d83a144b45f94eac60a95
Diamondooooo <almaz.sgv1@mail.ru>
2022-11-30 01:03:01 +0600

    final

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,28 @@
-import {AppBar, IconButton, Toolbar} from '@material-ui/core';
+import { AppBar, IconButton, Toolbar } from '@material-ui/core';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
 import { Link } from '@mui/material';
 import React from 'react';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-    const navigate = useNavigate()
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Link onClick={() => navigate('/')} variant="h6" component="span" style={{textDecoration: 'none', color: 'white', flexGrow: 1}}>
-                    Marvel
-                </Link>
-                <IconButton color="inherit">
-                    <BookmarkIcon onClick={() => navigate('/favorite')}/>
-                </IconButton>
-            </Toolbar>
-        </AppBar>
-    );
+  const navigate = useNavigate();
+  return (
+    <AppBar position='static'>
+      <Toolbar>
+        <Link
+          onClick={() => navigate('/')}
+          variant='h6'
+          component='span'
+          style={{ textDecoration: 'none', cursor: 'pointer', color: 'white', flexGrow: 1 }}
+        >
+          Marvel
+        </Link>
+        <IconButton color='inherit'>
+          <BookmarkIcon onClick={() => navigate('/favorite')} />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
